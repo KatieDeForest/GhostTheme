1296038e6f9506a2be470a3a4153e9008b5fa763
KatieDeForest <KatieDeForest@outlook.com>
2024-06-17 01:06:08 +0000

    Hamburger menu 2.0

--- content/themes/katiedeforest/assets/js/hamburgermenu.js
+++ content/themes/katiedeforest/assets/js/hamburgermenu.js
@@ -1,19 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerMenu = document.getElementById('hamburger-menu');
-    const navMenu = document.getElementById('nav-menu');
-    const iconX = document.getElementById('icon-x');
-
-    hamburgerMenu.addEventListener('click', function () {
-        // Toggle open/close classes for hamburger menu
-        hamburgerMenu.classList.toggle('open');
-        
-        // Toggle visibility of nav menu
-        if (navMenu.classList.contains('open')) {
-            navMenu.classList.remove('open');
-            iconX.style.display = 'none'; // Hide X icon
-        } else {
-            navMenu.classList.add('open');
-            iconX.style.display = 'block'; // Show X icon
-        }
-    });
+document.getElementById('hamburger').addEventListener('click', function () {
+    this.classList.toggle('open');
+    document.getElementById('menu').classList.toggle('open');
 });
